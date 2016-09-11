1fe9de316213b10eba2ba3af4cf35d536162fe2c
William <williamstogin2018@u.northwestern.edu>
2016-09-11 15:49:47 -0500

    Broken version do not checkout

--- app/app.service.ts
+++ app/app.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Player} from './player/player';
 
 @Injectable()
 export class AppService {
+playerList: Player[] = []
+
+
     getPlayerList(): Promise<string[]> {
-        return Promise.resolve(["Hi", "mom"]);
+        return Promise.resolve(playerList);
     }
+
+    // Adds a player string to the list if it does not already exist
+    // return: true if the player did not already exist, false otherwise
+    addPlayer(): Promise<boolean> {
+
+    }
+
+
+
+
 
 }
