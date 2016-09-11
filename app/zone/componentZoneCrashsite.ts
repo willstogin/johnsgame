0f2f70e46986805c0deeeba869bae8cb91c30735
William <williamstogin2018@u.northwestern.edu>
2016-09-11 15:10:19 -0500

    Reset componentZoneCrashsite.ts to the master version

--- app/zone/componentZoneCrashsite.ts
+++ app/zone/componentZoneCrashsite.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { AppService } from '../app.service';
 
 @Component({
     selector: 'component-zone-crashsite',
-    providers: [AppService],
-    template: `<h1>My First Angular 2 App HI</h1>
-    <h2>Players: {{players}}
+    template: `<h1>My First Angular 2 App HI MOM</h1>
                `
 })
-export class ComponentZoneCrashsite {
-    players: string[];
-
-    constructor(private appService: AppService) {}
-
-    getPlayers(): void {
-        this.appService.getPlayerList().then(players => this.players = players)
-    }
-
-    ngOnInit(): void {
-        this.getPlayers();
-    }
-}
-
+export class ComponentZoneCrashsite { }
