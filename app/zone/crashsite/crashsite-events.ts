473cc0dc348d65acb469846c491030492851237b
William <williamstogin2018@u.northwestern.edu>
2016-09-11 18:19:33 -0500

    Transcribed the zone a events

---
+++ app/zone/crashsite/crashsite-events.ts
@@ -0,0 +1,24 @@
+// 2: Danger! Someone in your group realized that they broke their arm during
+//      the crash. If no one in the group has a first-aid kit, that person must
+//      rest for two hours. The first-aid kit can still be used afterward.
+// 3: You found a large first-aid kit. Use it once to give one health to every
+//      one in your group.
+// 4: Your group found a pack of small flares! Each person can only cary one flare.
+//      Flares can be used to notify everyone in the zone where you are. Then
+//      anyone in the zone can take a two-hour trek to get to that location.
+// 5: Danger! A storm is coming. If any player is not sheltered in two hours,
+//      then they lose 2 health. If they do take shelter, they lose that turn.
+// 6: Landmark-Zone B: Entrance to the woods. There is a bridge to the entrance. For
+//      each person in the group, there are 1:6 odds that they get hurt crossing
+//      the bridge and lose 2 health. In that case, everyone else loses 1 health
+//      trying to help.
+// 7: Landmark-Shelter: Crash Site. You can replenish your health and water. You
+//      can also find shelter at this site.
+// 8: Landmark: A clearing. There is nothing here.
+// 9: Danger! BEAR ATTACK! Everyone can immediately decide to split into groups.
+//      The largest remaining group gets chased by a bear. For a group of size
+//      n, odds are 1:n+1 that nobody gets hurt.
+// 10: Landmark-Zone C: Foot of the mountain. Decide if you want to enter Zone C
+// 11: You salvage some airplane parts to build a travel tent. Each person can
+//      only cary one part and you must have to parts to make a shelter.
+// 12: Landmark-Zone B: Entrance to the woods.
