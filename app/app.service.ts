e5a628bff07f293b60837879e321f03fe3a2350d
William <williamstogin2018@u.northwestern.edu>
2016-09-11 16:24:27 -0500

    Added weather

--- app/app.service.ts
+++ app/app.service.ts
@@ -2,8 +2,24 @@ import { Injectable } from '@angular/core';
 import {Player} from './player/player';
 import {Group} from './group/group';
 
+enum WeatherStates {
+    CLEAR,          // Default state of the weather
+    PARTLY_CLOUDY,  // When someone triggers a storm, the weather is
+                    // PARTLY_CLOUDY next turn
+    CLOUDY,         // Next progression after PARTLY_CLOUDY
+    STORMING        // Next progression after CLOUDY, return to CLEAR after
+}
+
 @Injectable()
 export class AppService {
+
+    groupList: Array<Group> = new Array<Group>();
+    weather: WeatherStates = WeatherStates.CLEAR;
+
+
+
+
+
     group: Group = new Group();
 
 
@@ -13,12 +29,20 @@ export class AppService {
         return Promise.resolve(this.group);
     }
 
-    // Adds a player string to the list if it does not already exist
+    // Adds a player to the group if it does not already exist
     // return: true if the player did not already exist, false otherwise
     addPlayer(player: Player): Promise<boolean> {
 
 
         return Promise.resolve(false);
     }
+
+
+
+
+
+
+
+
 
 }
