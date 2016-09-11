fc2e6830a313df3b0c8b5037f2a1b72f001821af
Henry Zimmerman <zimhen7@gmail.com>
2016-09-11 14:38:51 -0500

    added routing and a zone component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from './app.routing';
+import { ComponentZoneCrashsite } from './zone/componentZoneCrashsite';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule, routing ],
+  declarations: [ AppComponent, ComponentZoneCrashsite ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
