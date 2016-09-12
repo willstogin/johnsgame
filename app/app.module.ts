8e5b91c216b7cb848c6ed2ed8ebd9dced66ba91c
Henry Zimmerman <zimhen7@gmail.com>
2016-09-11 19:12:13 -0500

    login almost works

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,14 +6,16 @@ import { ComponentPlayer } from './player/componentPlayer';
 import { ComponentGroup } from './group/componentGroup';
 import { ComponentEvent } from './event/componentEvent';
 import { ComponentChoice } from './event/componentChoice';
+import { ComponentLogin } from './login/componentLogin';
+import { FormsModule } from '@angular/forms';
 
 
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule, routing ],
-  declarations: [ AppComponent, ComponentZoneCrashsite, ComponentPlayer, ComponentGroup, ComponentEvent, ComponentChoice ],
+  imports: [ BrowserModule, routing, FormsModule ],
+  declarations: [ AppComponent, ComponentZoneCrashsite, ComponentPlayer, ComponentGroup, ComponentEvent, ComponentChoice, ComponentLogin ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/login/componentLogin.ts
+++ app/login/componentLogin.ts
@@ -1,3 +1,38 @@
-export class ComponentLogin {
+import { Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { Player } from '../player/player';
 
+
+@Component({
+    selector: 'component-login',
+    providers: [AppService],
+    template: `
+ <form (ngSubmit)="onSubmit()">
+      <div class="form-group">
+        <label for="name">Username</label>
+        <input type="text" class="form-control" id="name" required
+        [(ngModel)]="model.name" name="name"
+        >
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+</form>
+{{diagnostic}}
+    `
+
+
+})
+
+
+export class ComponentLogin {
+    model = new Player('');
+    private _appService: AppService;
+
+    constructor(appService: AppService) {
+        this._appService = appService;
+    }
+
+    private onSubmit(): void {
+        this._appService.addPlayer(this.model);
+    }
+    get diagnostic() { return JSON.stringify(this.model); }
 }
